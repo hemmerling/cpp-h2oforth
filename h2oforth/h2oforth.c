29a4f5694f5d7f193d5119ecd6980907ae3b960a
hemmerling <hemmerling@gmx.net>
2022-02-18 20:31:15 +0100

    Update

--- h2oforth/h2oforth.c
+++ h2oforth/h2oforth.c
@@ -220,7 +220,7 @@ static /*const */ char aListofBinary[] = { '-', '0', '1', ',', '.' };
 static /*const */ char aListofOctal[] = { '-', '0', '1', '2', '3', '4', '5', '6', '7', ',', '.' };
 static /*const */ char aListofDecimal[] = { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.' };
 static /*const */ char aListofHex[] = { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', ',', '.' };
-static /*const */ char aListOfBase[NUMBERTABLE_SIZE] = { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+static /*const */ char aListOfAllBases[NUMBERTABLE_SIZE] = { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ',', '.' };
 static /*const */ char aListofFloat[] = { '-', '+', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'E', ',' };
@@ -455,7 +455,7 @@ int isSPInteger(void) {
 		break;
 	default:
 		aListPointer = aListOfBase;
-		lenAllowedCharactersBuffer = sizeof(aListOfBase);
+		lenAllowedCharactersBuffer = sizeof(aListOfAllBases);
 		return(result);
 	};
 
@@ -523,6 +523,8 @@ void storeSPInteger(void) {
 		lenAllowedCharactersBuffer = sizeof(aListofHex);
 		break;
 	default:
+		aListPointer = aListOfBase;
+		lenAllowedCharactersBuffer = sizeof(aListOfAllBases);
 		return;
 	};
 
@@ -635,6 +637,8 @@ int isDPInteger(void) {
 		lenAllowedCharactersBuffer = sizeof(aListofHex);
 		break;
 	default:
+		aListPointer = aListOfBase;
+		lenAllowedCharactersBuffer = sizeof(aListOfAllBases);
 		return(result);
 	};
 	/* Don't proceed if just 1 character => "." is necessary, but is no valid Double Precision Integer */
@@ -706,6 +710,8 @@ void storeDPInteger(void) {
 		lenAllowedCharactersBuffer = sizeof(aListofHex);
 		break;
 	default:
+		aListPointer = aListOfBase;
+		lenAllowedCharactersBuffer = sizeof(aListOfAllBases);
 		return;
 	};
 	/* Don't proceed if just 1 character => "." is necessary, but is no valid Double Precision Integer */
@@ -783,7 +789,6 @@ void storeDPInteger(void) {
 /* I was told: According to the ANS standard, floating-point numbers are always
    interpreted as decimal (regardless of the content of the BASE variable) */
 #ifdef FLOAT_SUPPORT
-
 int isFloat(void) {
 	int result = FALSE;
 	int aWordIndex = 0;
